e1b07d581052f7e14b1d5a941708a70ea4fc20f9
XiangMang <1754536393@qq.com>
2023-05-07 12:28:37 +0800

    注册驱动

--- src/com/jdbc/Jdbc01.java
+++ src/com/jdbc/Jdbc01.java
@@ -1,12 +1,19 @@
 package com.jdbc;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.SQLException;
+
 /**
  * 完成简单从操作
  */
 public class Jdbc01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // 1. 注册驱动
+        Driver driver = new Driver();
+
         // 2. 得到连接
+        String url = ""
         // 3. 执行sql
         // 4. 关闭连接资源
     }
