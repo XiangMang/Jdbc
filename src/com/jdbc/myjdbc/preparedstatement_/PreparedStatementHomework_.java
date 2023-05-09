a935e88a9c05c64d5a02cad0432693552cca768a
XiangMang <1754536393@qq.com>
2023-05-09 15:03:07 +0800

    修改 PreparedStatementHomework

--- src/com/jdbc/myjdbc/preparedstatement_/PreparedStatementHomework_.java
+++ src/com/jdbc/myjdbc/preparedstatement_/PreparedStatementHomework_.java
@@ -2,13 +2,13 @@ package com.jdbc.myjdbc.preparedstatement_;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.jdbc.utils.JDBCUtils;
 
 public class PreparedStatementHomework_ {
     public static void main(String[] args) {
-        Connection connection = null;
 
         String createTable = "CREATE TABLE admin (" +
                 "id INT PRIMARY KEY," +
@@ -25,7 +25,11 @@ public class PreparedStatementHomework_ {
 
         String delete = "DELETE FROM jdbcTest.admin WHERE id = 2";
 
+        String showAdmin = "SELECT * FROM jdbcTest.admin";
+
+        Connection connection = null;
         PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
         try {
             connection = JDBCUtils.getConnection();
@@ -40,10 +44,17 @@ public class PreparedStatementHomework_ {
 
             preparedStatement = connection.prepareStatement(delete);
             preparedStatement.executeUpdate();
+
+            resultSet = preparedStatement.executeQuery(showAdmin);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                System.out.println(id + "\t" + username);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
-            JDBCUtils.close(null, preparedStatement, connection);
+            JDBCUtils.close(resultSet, preparedStatement, connection);
         }
 
     }
